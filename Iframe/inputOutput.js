1fa6f8181cef4628d14f650be00d72bbda38e678
Lucas Lorenço Alves <58004390+GitHubAlves150@users.noreply.github.com>
2025-03-26 22:59:41 -0300

    Full comit

---
+++ Iframe/inputOutput.js
@@ -0,0 +1,33 @@
+
+const calc = document.getElementById('calcular');
+nome.value="";
+altura.value="";
+peso.value="";
+
+//
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const result = document.getElementById('resultado');
+
+    if (nome !== "" && altura !== "" && peso !== "") 
+    {
+const imc =(peso/(altura * altura) ) ;
+        //result.textContent= nome+" tem "+peso+"Kg"+" e mede "+altura+" de altura."+ " E seu imc é: "+imc.toFixed(2); 
+        //OU
+        result.textContent= `${nome} tem ${peso}Kg e mede ${altura} de altura.E seu imc é: ${imc.toFixed(2)}`; 
+    }
+    else
+    {
+        window.alert("Existem campos vazios");
+    }
+    
+}
+calc.addEventListener('click', imc);
+
+
+
+
+//Referencia
+//https://www.youtube.com/watch?v=RacwEvoTz_Y&list=PLDgemkIT111AzoS1rB61sgMJbsEA4pyD2
