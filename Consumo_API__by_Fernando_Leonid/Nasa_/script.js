80279e96de65fbe8444bc5ab9faf90d040347c58
Lucas Lorenço Alves <58004390+GitHubAlves150@users.noreply.github.com>
2025-05-05 19:02:21 -0300

    full comit

--- Consumo_API__by_Fernando_Leonid/Nasa_/script.js
+++ Consumo_API__by_Fernando_Leonid/Nasa_/script.js
@@ -10,55 +10,67 @@ async function nasaDate() {
         const data = await response.json();
 
         /*DOM*/
-        const JSO=data.sol_keys[6];
-        console.log(":::", data);
+        const meses = ["Default", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+        const JSO = data.sol_keys[6];
+        console.log(":::", JSO, meses[1]);
         /*document.getElementById('mes_').textContent = data[JSO].Month_ordinal;*/
-       let mes= data[JSO].Month_ordinal;
-        switch (mes) {
-            case 1:                
-                console.log("Janeiro", mes);
-            break;
-            case 2:
-                console.log("Fevereiro", mes);
-            break;
-            case 3:
-                console.log("Março", mes);
-            break;
-            case 4:
-                console.log("Abril", mes);
-            break;
-            case 5:
-                console.log("Maio", mes);
-            break;
-            case 6:
-                console.log("Junho", mes);
-            break;
-            case 7:
-                console.log("Julho", mes);
-            break;
-            case 8:
-                console.log("Agosto", mes);
-            break;
-            case 9:
-                console.log("Setembro", mes);
-            break;
-            case 10:
-                console.log("Outubro", mes);
-            break;
-            case 11:
-                
-                document.getElementById('sol_atual').textContent=JSO;   
-                const diaMarciano= data[JSO].Last_UTC;             
-                document.getElementById('mes_').textContent="Novembro "+diaMarciano.slice(8, 10);
-                document.getElementById('High_temp').textContent= data[JSO].AT.mx;
-                document.getElementById('Low_temp').textContent= data[JSO].AT.mn;
-                
+        let mes = data[JSO].Month_ordinal;
+
+        console.log(data)
+        for (let c = 1; c <= 12; c++) {
+            if (c === mes) {
+                document.getElementById('sol_atual').textContent = JSO;
+                const diaMarciano0 = data[JSO].Last_UTC;
+                document.getElementById('mes_').textContent = meses[c] + " " + diaMarciano0.slice(8, 10);
+                document.getElementById('High_temp').textContent = data[JSO].AT.mx;
+                document.getElementById('Low_temp').textContent = data[JSO].AT.mn;
                 console.log("Novembro", mes);
-            break;
-            case 12:
-                console.log("Dezembro", mes);
-            break;
+                /*=========semanais==========*/
+                /*Monday*/
+                document.getElementById('monday').textContent = "Sol " + (JSO - 6);
+                const diaMarciano1 = data[(JSO - 6)].Last_UTC;
+                document.getElementById('mes_monday').textContent = meses[c] + " " + diaMarciano1.slice(8, 10);
+                document.getElementById('High_temp_monday').textContent = data[(JSO - 6)].AT.mx;
+                document.getElementById('Low_temp_monday').textContent = data[(JSO - 6)].AT.mn;
+                /*Tuesday*/
+                document.getElementById('Tuesday').textContent = "Sol " + (JSO - 5);
+                const diaMarciano2 = data[(JSO - 5)].Last_UTC;
+                document.getElementById('mes_Tuesday').textContent = meses[c] + " " + diaMarciano2.slice(8, 10);
+                document.getElementById('High_temp_Tuesday').textContent = data[(JSO - 5)].AT.mx;
+                document.getElementById('Low_temp_Tuesday').textContent = data[(JSO - 5)].AT.mn;
+                /*Wednesday*/
+                document.getElementById('Wednesday').textContent = "Sol " + (JSO - 4);
+                const diaMarciano3 = data[(JSO - 4)].Last_UTC;
+                document.getElementById('mes_Wednesday').textContent = meses[c] + " " + diaMarciano3.slice(8, 10);
+                document.getElementById('High_temp_Wednesday').textContent = data[(JSO - 4)].AT.mx;
+                document.getElementById('Low_temp_Wednesday').textContent = data[(JSO - 4)].AT.mn;
+                /*Thursday*/
+                document.getElementById('Thursday').textContent = "Sol " + (JSO - 3);
+                const diaMarciano4 = data[(JSO - 3)].Last_UTC;
+                document.getElementById('mes_Thursday').textContent = meses[c] + " " + diaMarciano4.slice(8, 10);
+                document.getElementById('High_temp_Thursday').textContent = data[(JSO - 3)].AT.mx;
+                document.getElementById('Low_temp_Thursday').textContent = data[(JSO - 3)].AT.mn;
+                /*Fryday*/
+                document.getElementById('Fryday').textContent = "Sol " + (JSO - 2);
+                const diaMarciano5 = data[(JSO - 2)].Last_UTC;
+                document.getElementById('mes_Fryday').textContent = meses[c] + " " + diaMarciano5.slice(8, 10);
+                document.getElementById('High_temp_Fryday').textContent = data[(JSO - 2)].AT.mx;
+                document.getElementById('Low_temp_Fryday').textContent = data[(JSO - 2)].AT.mn;
+                /*Satuday*/
+                document.getElementById('Satuday').textContent = "Sol " + (JSO - 1);
+                const diaMarciano6 = data[(JSO - 1)].Last_UTC;
+                document.getElementById('mes_Satuday').textContent = meses[c] + " " + diaMarciano6.slice(8, 10);
+                document.getElementById('High_temp_Satuday').textContent = data[(JSO - 1)].AT.mx;
+                document.getElementById('Low_temp_Satuday').textContent = data[(JSO - 1)].AT.mn;
+                /*Sunday */
+            }
+
+
+
         }
+
+
+
 
         /*Console*/
 
