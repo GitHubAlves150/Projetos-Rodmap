ad06b413eb92fa8e89313809158fdd0998a8a978
Lucas Lorenço Alves <58004390+GitHubAlves150@users.noreply.github.com>
2025-05-08 20:15:24 -0300

    Full comit

--- Consumo_API__by_Fernando_Leonid/Nasa_/script_dolar.js
+++ Consumo_API__by_Fernando_Leonid/Nasa_/script_dolar.js
@@ -1,3 +1,17 @@
 'user strict'
+const dolarNUmber= document.getElementById('dolar_').value;
 
 
+async function Dolar() {
+    const url = `https://economia.awesomeapi.com.br/last/USD-BRL/`;
+    const response= await fetch(url);
+    const data = await response.json();
+
+    document.getElementById('USD-BRD').textContent = data.USDBRL.bid +" Real Brasileiro";
+    document.getElementById('data_dolar').textContent = data.USDBRL.create_date;
+
+    console.log(">>", dolarNUmber);
+    
+}
+
+Dolar();
