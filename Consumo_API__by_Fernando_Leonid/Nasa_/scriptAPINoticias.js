23b790d34b4de53309e1a2d5b9928c6b9e617ed8
Lucas Lorenço Alves <58004390+GitHubAlves150@users.noreply.github.com>
2025-05-12 20:39:17 -0300

    full comit

--- Consumo_API__by_Fernando_Leonid/Nasa_/scriptAPINoticias.js
+++ Consumo_API__by_Fernando_Leonid/Nasa_/scriptAPINoticias.js
@@ -1,10 +1,6 @@
 'use strict'
 
 const img_NoticeA = document.getElementById('Imagem-A');
-const link_NoticeA = document.getElementById('Imagem-A');
-
-
-
 const img_NoticeB = document.getElementById('Imagem-B');
 const img_NoticeC = document.getElementById('Imagem-C');
 
@@ -12,34 +8,46 @@ const manchetA = document.getElementById('manchet-A');
 const manchetB = document.getElementById('manchet-B');
 const manchetC = document.getElementById('manchet-C');
 
+const link1 = document.getElementById('link_1');
+const IMGlink1 = document.getElementById('Img_1');
 
+
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
 async function NoticiasNew() {
-  const APIKey = 'pub_851015f2be20baf5a5979dc3d5594ef2502b1';
-  const url = `https://newsdata.io/api/1/latest?apikey=${APIKey}&q=tesla&language=en`;
+    const APIKey = '2708f2cc67b64745be4c29e748e7277d';
+    const url = `https://api.worldnewsapi.com/search-news?api-key=${APIKey}&text=tesla`;
+    /*  const url= `https://gnews.io/api/v4/search?q=example&apikey=6d6bdcface1b61181abcfbc925d9022f`; */
 
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
 
-    img_NoticeA.src = data.results[4].image_url;
-    manchetA.textContent= data.results[4].title;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
+        img_NoticeA.src = data.news[3].image;
+        manchetA.textContent = data.news[3].title;
 
-    img_NoticeB.src = data.results[5].image_url;
-    manchetB.textContent= data.results[5].title;
+        img_NoticeB.src = data.news[1].image;
+        manchetB.textContent = data.news[1].title;
 
-    img_NoticeC.src = data.results[9].image_url;
-    manchetC.textContent= data.results[9].title;
+        img_NoticeC.src = data.news[2].image;
+        manchetC.textContent = data.news[2].title;
 
-  console.log("Noticia..", data.results[9].title);
+        await delay(4000);
+        link1.href = data.news[3].url;
+        IMGlink1.src = data.news[3].image;
 
-  }
-  catch (error) {
-    alert("nop");
-  }
+        console.log("Google News..", data.news[3]);
+
+    }
+    catch (error) {
+        alert("Ecedeu as requisições da API");
+    }
 
 }
 
