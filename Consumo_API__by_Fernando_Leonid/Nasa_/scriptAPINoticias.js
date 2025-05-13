02eb8cf883d520baecc4b40d11b0f2002494a6c9
Lucas Lorenço Alves <58004390+GitHubAlves150@users.noreply.github.com>
2025-05-13 13:36:59 -0300

    Full Comit

--- Consumo_API__by_Fernando_Leonid/Nasa_/scriptAPINoticias.js
+++ Consumo_API__by_Fernando_Leonid/Nasa_/scriptAPINoticias.js
@@ -10,6 +10,10 @@ const manchetC = document.getElementById('manchet-C');
 
 const link1 = document.getElementById('link_1');
 const IMGlink1 = document.getElementById('Img_1');
+const NoticeDesc_ = document.getElementById('Descricaonoticias');
+const TituloDesc_ = document.getElementById('TituloDesc');
+
+let string = "";
 
 
 
@@ -19,9 +23,9 @@ function delay(ms) {
 
 
 async function NoticiasNew() {
-    const APIKey = '2708f2cc67b64745be4c29e748e7277d';
-    const url = `https://api.worldnewsapi.com/search-news?api-key=${APIKey}&text=tesla`;
-    /*  const url= `https://gnews.io/api/v4/search?q=example&apikey=6d6bdcface1b61181abcfbc925d9022f`; */
+     /*const APIKey = '2708f2cc67b64745be4c29e748e7277d';
+     const url = `https://api.worldnewsapi.com/search-news?api-key=${APIKey}&text=tesla`;
+     /*  const url= `https://gnews.io/api/v4/search?q=example&apikey=6d6bdcface1b61181abcfbc925d9022f`; */
 
 
 
@@ -38,15 +42,30 @@ async function NoticiasNew() {
         img_NoticeC.src = data.news[2].image;
         manchetC.textContent = data.news[2].title;
 
-        await delay(4000);
-        link1.href = data.news[3].url;
-        IMGlink1.src = data.news[3].image;
+        for (let c = 0; c < 3; c++) {
+            await delay(2000);
+            link1.href = data.news[4].url;
+            IMGlink1.src = data.news[4].image;
+            TituloDesc_.textContent = data.news[4].title;
+            string = data.news[4].text;
+            NoticeDesc_.textContent = string.slice(0, 300)+ '...';
+            console.log("string..", string);
 
-        console.log("Google News..", data.news[3]);
+            await delay(2000);
+            link1.href = data.news[5].url;
+            IMGlink1.src = data.news[5].image;
+            TituloDesc_.textContent = data.news[5].title;
+            string = data.news[5].text;
+            NoticeDesc_.textContent = string.slice(0, 300) + '...' ;
+        }
 
     }
     catch (error) {
         alert("Ecedeu as requisições da API");
+        NoticeDesc_.textContent = string.slice(0, 30);
+
+
+
     }
 
 }
